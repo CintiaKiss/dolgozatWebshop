b3dcc98253f7d4734ddd87c1e3566f8f0bd8dd97
CintiaKiss <153748865+CintiaKiss@users.noreply.github.com>
2025-01-15 15:12:55 +0100

    Update environments.ts

--- src/app/environments.ts
+++ src/app/environments.ts
@@ -2,5 +2,12 @@ export const environments = {
     production: false,
     firebaseConfig: {
         databaseURL: "https://dolgozat-79584-default-rtdb.europe-west1.firebasedatabase.app./.json",
+            apiKey: "AIzaSyCkRi_bSnlQbGkMiSE3HkIhOqNh0bIOQmA",
+            authDomain: "dolgozatwebshop.firebaseapp.com",
+            projectId: "dolgozatwebshop",
+            storageBucket: "dolgozatwebshop.firebasestorage.app",
+            messagingSenderId: "441530538702",
+            appId: "1:441530538702:web:367d20593ed56116384e9f",
+            measurementId: "G-S6MC0N9K56"
         }
 };
